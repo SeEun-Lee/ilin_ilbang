39118232018cd0d7436503dbe990e71db90b537d
SeJin-Ahn <84254030+greetings07@users.noreply.github.com>
2021-11-01 14:47:31 +0900

    Add files via upload

--- src/main/java/com/ilin_ilbang/controller/MemberController.java
+++ src/main/java/com/ilin_ilbang/controller/MemberController.java
@@ -3,8 +3,12 @@ package com.ilin_ilbang.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.ilin_ilbang.domain.MemberVO;
 import com.ilin_ilbang.service.MemberService;
 
 import lombok.AllArgsConstructor;
@@ -18,10 +22,43 @@ public class MemberController {
 	
 	private MemberService service;
 	
-	@GetMapping("/list")
-	public void list(Model model) {
+	@PostMapping("/register")
+	public String register(MemberVO member, RedirectAttributes rttr) {
 		
-		log.info("list");
-		model.addAttribute("list", service.getList());
+		log.info("register: " + member);
+		
+		service.register(member);
+		rttr.addFlashAttribute("result", member.getMid());
+		
+		return "redirect:/member/login";
+	}
+	@GetMapping("/get")
+	public void get(@RequestParam("mid") String mid, Model model) {
+		
+		log.info("/get");
+		
+		model.addAttribute("member", service.get(mid));
+	}
+	@PostMapping("/modify")
+	public String modify(MemberVO member, RedirectAttributes rttr) {
+		
+		log.info("modify: " + member);
+		
+		if (service.modify(member)) {
+			rttr.addAttribute("result", "success");
+		}
+		
+		return "redirect:/member/login";
+	}
+	@PostMapping("/remove")
+	public String remove(@RequestParam("mid") String mid, RedirectAttributes rttr) {
+		
+		log.info("remove... " + mid);
+		
+		if (service.remove(mid)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/member/login";
 	}
 }
